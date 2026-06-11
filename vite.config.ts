1679054da1c26f91b15480eacaa86c9f270dd908
Alfan Luky <kkijanaa@gmail.com>
2026-06-11 13:23:21 +0300

    Prepare website for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/african-bold-safari/',
+  base: '/AfricanBoldSafari/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
